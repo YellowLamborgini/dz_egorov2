ad607dc2a172ddc73c6e1b2311feaf82486518c7
YellowLamborgini <denisegorov2011@yandex.ru>
2023-10-30 21:42:32 +0300

    Добавьте файлы проекта.

---
+++ dz_egorov2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число и процент");
+double chislo = Convert.ToInt32(Console.ReadLine());
+int procent = Convert.ToInt32(Console.ReadLine());
+double result;
+if (procent < 1 | procent > 100)
+{
+    Console.WriteLine("Ошибка");
+}
+else
+{
+    result = chislo * (procent / 100.0);
+    Console.WriteLine($"Результат равен: {result}");
+
+}
